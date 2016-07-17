d0b0d329c1e354be3ce214fbef82b595f9a034a3
lilo <martin16@sina.cn>
2016-07-17 06:18:22 +0000

    调整服务器端项目结构，追加提交。

---
+++ src/index/controller.js
@@ -0,0 +1,12 @@
+/**
+ * Build Date: 06-29-2016
+ * Copyright (c): LHS Develop Group
+ * Author: lhs
+ */
+module.exports = {
+    pageIndex: pageIndex
+};
+
+function pageIndex (req, res) {
+    res.render('index');
+}
