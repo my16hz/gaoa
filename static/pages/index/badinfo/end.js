f1a9b28ca55711609e655ba63d72220968070e6f
lilo <martin16@sina.cn>
2016-09-04 08:18:58 +0000

    [客户端] 点击菜单时，默认打开首个可访问菜单的页面。

--- static/pages/index/badinfo/end.js
+++ static/pages/index/badinfo/end.js
@@ -1,5 +1,7 @@
     $('body').ready(function () {
-        $('#lhs_aside_menu > li').bind('click', function () {
+        var jqli = $('#lhs_aside_menu > li');
+
+        jqli.bind('click', function () {
             $('#main_panel').empty();
             $(this).addClass('selected').siblings('li').removeClass('selected');
 
@@ -15,6 +17,6 @@
             }
         });
 
-        BadInfoRecordPage.run();
+        jqli.eq(0).click();
     });
 })();
--- static/pages/index/smartoffice/end.js
+++ static/pages/index/smartoffice/end.js
@@ -1,5 +1,7 @@
     $('body').ready(function () {
-        $('#lhs_aside_menu > li').bind('click', function () {
+        var jqli = $('#lhs_aside_menu > li');
+
+        jqli.bind('click', function () {
             $('#main_panel').empty();
             $(this).addClass('selected').siblings('li').removeClass('selected');
 
@@ -13,6 +15,6 @@
             }
         });
 
-        LHSAllMessagesPage.run();
+        jqli.eq(0).click();
     });
 })();
