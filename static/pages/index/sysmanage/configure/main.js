2883e065ebd28af8fbaf30c404116aa02bac7352
my16hz <martin16.lilo@gmail.com>
2016-10-06 16:07:21 +0800

    [Client End] Refactoriing system config page.

--- static/pages/index/sysmanage/configure/main.js
+++ static/pages/index/sysmanage/configure/main.js
@@ -5,133 +5,69 @@
  */
 var LHSConfigurePage = $.extend({}, LHSBasicPage, {
     run: function () {
+        var self = this;
         /*inject:jqtmpl:html*/
         /*endinject*/
 
-
         $(this.el).empty().append(jqtmpl($, {data: {}}).join(''));
-        this.initDependencies()
-            ._drawDataTable();
+
+        this.initDependencies();
+
+        this.dataTable = this._createTable('#tableWrapper', '/sysmanage/configure', [
+            {title: '配置项', field: 'name'},
+            {title: '当前值', field: 'value'},
+            {
+                title: '操作', field: 'action',
+                formatter: function () {
+                    return '<a href="javascript:" title="修改"><i class="glyphicon glyphicon-edit"></i></a>';
+                },
+                events: {
+                    'click a:first': function () {
+                        var modal = $('#dataModal');
+                        var config = arguments[2];
+
+                        self._setFormControlValues(modal.find('form'), config);
+
+                        modal.find('.control-label').text(config.name + ':');
+                        modal.parent().removeClass('hide');
+                    }
+                }
+            }
+        ]);
     },
     events: {
-        'click #configModal .btn-default': 'closeModal',
-        'click #configModal .btn-primary': 'updateConfigure'
+        'click #dataModal .btn-default': 'closeDataModal',
+        'click #dataModal .btn-primary': 'saveConfig'
     },
-    _drawDataTable: function () {
+    closeDataModal: function () {
+        var modal = $('#dataModal');
+
+        this._clearFormControlValues(modal.find('form'));
+
+        modal.find('.control-label').empty();
+        modal.parent().addClass('hide');
+    },
+    saveConfig: function () {
         var self = this;
 
-        if (!this.dataTable) {
-            (this.dataTable = $('#dataTable')).bootstrapTable({
-                method: 'get',
-                url: '/sysmanage/configure',
-                cache: false,
-                ajaxOptions: {
-                    beforeSend: function () {
-                        self._showLoading();
-                    },
-                    complete: function () {
-                        self._removeLoading();
-                    }
-                },
-                onLoadError: function (xhr) {
-                    self._showXHRError(xhr.responseText);
-                },
-                columns: [{
-                    title: '配置项',
-                    field: 'name'
-                }, {
-                    title: '当前值',
-                    field: 'value'
-                }, {
-                    title: '操作',
-                    field: 'action',
-                    formatter: function () {
-                        return '<a href="javascript:" title="修改">' +
-                            '<i class="glyphicon glyphicon-edit"></i></a>';
-                    },
-                    events: {
-                        'click a:first': function () {
-                            self._showDetailModal(arguments[2]);
-                        }
-                    }
-                }]
-            }, this._buildTableOptions());
-        } else {
-            this._refreshTable();
-        }
-
-        return this;
-    },
-    _refreshTable: function () {
-        this.dataTable.bootstrapTable('refresh');
-
-        return this;
-    },
-    _showGridWrapper: function () {
-        $('#gridWrapper > div:first')
-            .next()
-            .removeClass('hide');
-
-        return this;
-    },
-    _hideGridWrapper: function () {
-        $('#gridWrapper > div:first')
-            .next()
-            .addClass('hide');
-
-        return this;
-    },
-    showDataModal: function () {
-        var self = this;
-        $('#configModal').removeClass('hide').siblings().addClass('hide');
-        this._clearFormControlValues($('#configModal form'));
-
-        this._shrinkTable()
-            ._showGridWrapper();
-    },
-    _showDetailModal: function (config) {
-        var jqform = $('#configModal form');
-        this._setFormControlValues(jqform, config);
-        this._showGridWrapper();
-    },
-    _buildTableOptions: function () {
-        var self = this;
-
-        return {
-            method: 'get',
-            url: '/sysmanage/configure',
-            cache: false,
-            ajaxOptions: {
-                beforeSend: function () {
-                    self._showLoading();
-                },
-                complete: function () {
-                    self._removeLoading();
-                }
-            },
-            onLoadError: function (xhr) {
-                self._showXHRError('请求失败:' + xhr.responseText);
-            }
-        }
-    },
-    closeModal: function () {
-        this._hideGridWrapper();
-    },
-    updateConfigure: function () {
-        var self = this;
         this._sendRequest({
             type: 'post',
             url: '/sysmanage/configure/update',
-            validator: $.proxy(this._configValidator, this),
+            validator: $.proxy(this._validator, this),
             done: function () {
-                self._refreshTable().closeModal();
+                self.dataTable.refresh();
+                self.closeDataModal();
             }
         });
     },
-    _configValidator: function () {
-        var jqform = $('#configModal form');
-        var values = this._getFormControlValues(jqform);
 
-        return  values;
+    _validator: function () {
+        var values = this._validate($('#dataModal form'), {
+            value: function (val) {
+                if (!/^\d+$/.test(val)) return '配置项必须为数字。';
+            }
+        });
+
+        return values || false;
     }
 });
