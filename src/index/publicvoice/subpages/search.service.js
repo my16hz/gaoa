f85b40deb6c81e4b0d74f8cfd13f9650cfec51c0
lilo <martin16@sina.cn>
2016-11-26 13:43:22 +0000

    添加导出的查询条件

--- src/index/publicvoice/subpages/search.service.js
+++ src/index/publicvoice/subpages/search.service.js
@@ -65,5 +65,58 @@ function searchPubVoices (start, end, state, type, feedback, dispose, title, cal
 }
 
 function exportMatchedPubVoices (start, end, state, type, feedback, dispose, title, done) {
-    done();
+    var sql_stmt = "SELECT tb_publicvoice.*, tb_daily_pv.did, " +
+        "tb_pv_comment.from_user AS commnet_user,tb_pv_comment.comment AS pv_comment,tb_pv_comment.comment_date AS comment_date, " +
+        "tb_pv_feedback.content AS feedback_content,tb_pv_feedback.type AS feedback_type, tb_pv_feedback.createtime AS feedback_date " +
+        "FROM tb_publicvoice  " +
+        "LEFT JOIN tb_daily_pv ON tb_publicvoice.id = tb_daily_pv.pvid " +
+        "LEFT JOIN tb_pv_comment ON tb_publicvoice.id = tb_pv_comment.id " +
+        "LEFT JOIN tb_pv_feedback ON tb_publicvoice.id = tb_pv_feedback.id " +
+        "WHERE tb_publicvoice.createtime > @start AND tb_publicvoice.createtime < @end ";
+    var objParams = {
+        "start": start,
+        "end": end
+    };
+    if (state) {
+        sql_stmt += " AND tb_publicvoice.state = @state ";
+        objParams["state"] = state;
+    }
+    if (type) {
+        sql_stmt += " AND tb_publicvoice.type = @type ";
+        objParams["type"] = type;
+    }
+    if (feedback) {
+        sql_stmt += " AND tb_publicvoice.feedback_state = @feedback ";
+        objParams["feedback"] = feedback;
+    }
+    if (dispose) {
+        sql_stmt += " AND tb_publicvoice.dispose_stat = @dispose ";
+        objParams["dispose"] = dispose;
+    }
+    if (title) {
+        sql_stmt += " AND (tb_publicvoice.title LIKE @title OR tb_publicvoice.content LIKE @title) ";
+        objParams["title"] = '%' + title + '%';
+    }
+    sql_stmt += " ORDER BY tb_publicvoice.createtime DESC ";
+    console.log(sql_stmt);
+    var ps = dbpool.preparedStatement()
+        .input("start", sql.DateTime)
+        .input("end", sql.DateTime)
+        .input("state", sql.Int)
+        .input("feedback", sql.Int)
+        .input("dispose", sql.Int)
+        .input("type", sql.NVarChar)
+        .input("title", sql.NVarChar)
+        .prepare(sql_stmt, function (err) {
+            if (err) {
+                return callback(err, null);
+            }
+            ps.execute(objParams, function (err, recordset) {
+                callback(err, recordset)
+                ps.unprepare(function (err) {
+                    if (err)
+                        console.log(err);
+                });
+            });
+        });
 }
